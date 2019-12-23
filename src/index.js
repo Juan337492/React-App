f7492dff1eed2665c7da7e0e7446b75df070f230
Juan Rodriguez <Juan337492@yahoo.com>
2019-12-23 15:02:41 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import STORE from './store';
+
+
+ReactDOM.render(
+<App store={STORE}/>,
+ document.getElementById('root'),
+ );
+
+
